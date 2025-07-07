1724ee6d88dbff884e63dc1cf2ad4681aacd2822
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-07-08 01:22:43 +0800

    feat: add fetching functionality for getting lyrics data

---
+++ lyric_analyzer/fetcher.py
@@ -0,0 +1,64 @@
+import os
+from typing import TYPE_CHECKING
+
+from dotenv import load_dotenv
+from lyricsgenius import Genius
+
+if TYPE_CHECKING:
+    from lyricsgenius.types import Artist
+
+# Load genius access token from .env
+load_dotenv()
+TOKEN = os.getenv("GENIUS_ACCESS_TOKEN")
+
+# Set-up genius API
+genius: Genius = Genius(TOKEN)
+genius.remove_section_headers = True  # Remove section headers (e.g. [Chorus]) from lyrics when searching
+genius.skip_non_songs = False  # Include hits thought to be non-songs (e.g. track lists)
+genius.excluded_terms = ["(Remix)", "(Live)"]  # Exclude songs with these words in their title
+
+
+def get_artist_lyrics(artist_name: str, max_songs: int = 20) -> list[str]:
+    """Search for the first `max_songs` popular songs that the artist appears on.
+
+    Args:
+        artist_name (str): The name of the artist to search for.
+        max_songs (int, optional): The maximum number of songs to fetch. Defaults to 20.
+
+    Returns:
+        list[str]: A list of lyrics strings for the artist's songs.
+
+    """
+    artist: Artist | None = genius.search_artist(
+        artist_name,
+        max_songs=max_songs,
+        sort="popularity",
+        include_features=True,
+    )
+
+    return [song.lyrics for song in artist.songs] if artist else []
+
+
+def clean_lyrics(lyrics_list: list[str]) -> list[str]:
+    """Remove the first-line annotations that appear in every song.
+
+    Args:
+        lyrics_list (list[str]): List of lyrics strings.
+
+    Returns:
+        list[str]: List of cleaned lyrics strings with first-line annotations removed.
+
+    """
+    cleaned_lyrics_list = []
+    for lyrics in lyrics_list:
+        first_new_line = lyrics.index("\n")
+        cleaned_lyrics_list.append(lyrics[first_new_line + 1 :])
+    return cleaned_lyrics_list
+
+
+if __name__ == "__main__":
+    raw_lyrics_list = get_artist_lyrics("The Itchyworms", 5)
+    cleaned_lyrics_list = clean_lyrics(raw_lyrics_list)
+
+    for lyrics in cleaned_lyrics_list:
+        print(lyrics)
