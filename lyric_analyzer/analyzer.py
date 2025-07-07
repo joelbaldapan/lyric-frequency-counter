09895cdeb3581beb3ffaa692a1fdf9bd2209e2d8
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-07-08 03:21:40 +0800

    feat: add plotting functionality with matplotlib

--- lyric_analyzer/analyzer.py
+++ lyric_analyzer/analyzer.py
@@ -1,10 +1,13 @@
 from fetcher import get_artist_lyrics
+
 import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.probability import FreqDist
+
+import matplotlib.pyplot as plt
 
 nltk.download("stopwords")
 lemmatizer = WordNetLemmatizer()
@@ -23,11 +26,13 @@ def combine_lyrics(lyrics_list: list[str]) -> str:
 
 
 if __name__ == "__main__":
-    lyrics_list = get_artist_lyrics("Louie Zong", 4)
+    lyrics_list = get_artist_lyrics("Taylor Swift", 70)
     full_lyrics = combine_lyrics(lyrics_list)
 
     tokenized_lyrics = word_tokenize(full_lyrics)
     stop_words = set(stopwords.words("english"))
+    stop_words.update({",", "'s", "u", "'re", "'m", "'ll", "(", ")"})
+    stop_words -= {"him"}
 
     filtered_lyrics = [word for word in tokenized_lyrics if word.lower() not in stop_words]
     # filtered_lyrics = tokenized_lyrics
@@ -40,4 +45,16 @@ if __name__ == "__main__":
     sorted_freq = fdist.most_common()
 
     print(sorted_freq)
-    
+
+    top = 70
+    top_words = fdist.most_common(top)
+    words, counts = zip(*top_words)
+
+    plt.figure(figsize=(25, 6))
+    plt.bar(words, counts)
+    plt.title(f"Top {top} Most Frequent Words")
+    plt.ylabel("Frequency")
+    plt.xticks(rotation=45, ha="right")
+    plt.tight_layout()
+    plt.show()
+        
